cc82c83c24691bc0e9f508e238ddfa94285f01ff
Konstantin <neolit4@gmail.com>
2026-06-01 15:18:49 +0300

    fix: correct notification badge sync on dashboard load

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -341,7 +341,7 @@ const DashboardPage = {
         this._renderCalWidget(calEvents, today, scheduleEntries);
         this._renderImportantEvents(calEvents, today);
         this._renderAlerts(unackedDocs, recentNotifs);
-        UI.updateNotificationBadge(recentNotifs.length, true);
+        UI._setNotificationBadge(recentNotifs.length);
         this._renderBirthdays(birthdays);
         CompanyBirthdayModal._initDashboardChat();
 
